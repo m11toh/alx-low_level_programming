69b9954d1b50713634ebf3298c639c775bad2178
MARTIN MAINA <mashahkalipso@gmailcom>
2023-08-11 15:41:04 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,9 +4,9 @@
 #include <stdio.h>
 /* betty style doc for function main goes there */
 /**
- * main -  main function
+ * main - main function
  *
- * Return: Always 0
+ * Return: always 0
  */
 int main(void)
 {
